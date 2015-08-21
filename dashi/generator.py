cb1de4cc77e3368ed730c14044f76f2d20c3d909
Eli Ribble <eli@authentise.com>
2015-08-21 17:04:22 -0600

    Update more references to the old config style

--- dashi/generator.py
+++ dashi/generator.py
@@ -15,23 +15,24 @@ LOGGER = logging.getLogger(__name__)
 @asyncio.coroutine
 def go():
     config = dashi.config.parse()
-    template_loader = jinja2.FileSystemLoader(searchpath=config.template_path)
+    template_loader = jinja2.FileSystemLoader(searchpath=config['paths']['template'])
     template_environment = jinja2.Environment(loader=template_loader)
 
     connection = dashi.db.connection()
+
     authors = dashi.db.get_all_authors(connection)
-    print(authors)
-    return 
+    return
+
     LOGGER.debug(repo_stats)
     try:
-        os.mkdir(config.output_path)
+        os.mkdir(config['paths']['output'])
     except OSError:
         pass
 
     template = template_environment.get_template('index.html')
     output = template.render(repo_stats=repo_stats)
 
-    path = os.path.join(config.output_path, 'index.html')
+    path = os.path.join(config['paths']['output'], 'index.html')
     with open(path, 'w') as f:
         f.write(output)
 
